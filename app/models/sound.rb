e8bc5a47cc9d87e58cccf92b31959124c1868f94
flats <dave@robador.com>
2015-12-23 12:15:04 -0500

    content-type validation for sound

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -2,6 +2,6 @@ class Sound < ActiveRecord::Base
   include Paperclip::Glue
   belongs_to :user
   has_attached_file :soundfile
-  validates_attachment :soundfile, content_type: { content_type: ["application/pdf"] }
+  validates_attachment :soundfile, content_type: { content_type: ["audio/aac", "audio/mp4", "audio/mpeg", "audio/ogg", "audio/wav", "audio/webm"] }
   validates_attachment_presence :soundfile
 end
