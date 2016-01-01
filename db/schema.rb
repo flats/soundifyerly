8306db023251e24359bc67defc8525ff337961f4
flats <dave@robador.com>
2016-01-01 17:59:35 -0500

    migration for user reference in sound

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151231202310) do
+ActiveRecord::Schema.define(version: 20160101225512) do
 
   create_table "sounds", force: :cascade do |t|
-    t.string "title"
-    t.string "soundfile"
+    t.string  "title"
+    t.string  "soundfile"
+    t.integer "user_id"
   end
+
+  add_index "sounds", ["user_id"], name: "index_sounds_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string "username"
