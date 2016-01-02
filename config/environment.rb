7eb6f9532967fc42d21af26de2ad1e61bab3cbb2
flats <dave@robador.com>
2016-01-01 21:23:13 -0500

    modules into concerns directory, load in environment

--- config/environment.rb
+++ config/environment.rb
@@ -17,6 +17,7 @@ ActiveRecord::Base.raise_in_transactional_callbacks = true
 # $LOAD_PATH
 $: << '.'
 
+Dir["app/concerns/*.rb"].each { |f| require f }
 Dir["app/models/*.rb"].each { |f| require f }
 Dir["app/controllers/*.rb"].each { |f| require f }
 
