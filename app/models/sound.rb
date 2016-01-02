c0bc81d1e1a6a1670fb05fc9fa3ff570f942332b
flats <dave@robador.com>
2016-01-01 22:27:31 -0500

    user and sound validations for attribute presence

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -1,6 +1,7 @@
 class Sound < ActiveRecord::Base
   include Attachable
   belongs_to :user, inverse_of: :sounds
+  validates :title, presence: true
   before_destroy :destroy_soundfile
 
   def destroy_soundfile
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :sounds, inverse_of: :user, dependent: :destroy
   has_secure_password
-  validates :username, uniqueness: true
+  validates :username, uniqueness: true, presence: true
+  validates :real_name, presence: true
 end
